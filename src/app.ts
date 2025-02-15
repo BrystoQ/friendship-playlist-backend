1423bc5f9a747decfdd32ab1f47e04a2276af034
BrystoQ <quentin.aubert42@gmail.com>
2025-02-15 19:05:31 +0100

    feat(O2auth): authentification with Spotify

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
+// src/app.ts
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./config/db";
+import authRoutes from "./routes/auth.js";
 
+// Initialisation de l'application Express
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes de base
 app.get("/", (req, res) => {
   res.send("Bienvenue sur FriendShip Playlist API 🎵");
 });
 
-// Connexion à MongoDB
-connectDB();
+// Routes d'authentification
+app.use("/auth", authRoutes);
 
 export default app;
